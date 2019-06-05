93e02af91e8e0d3c6fe45cedaa260cc0ae74affa
vlineros <victorlineros@gmail.com>
2019-06-04 23:14:51 -0500

    html routes are linked exporting properly to server and displaying pages upon proper route input

--- data/friends.js
+++ data/friends.js
@@ -1,1 +1,10 @@
 // This simply stores the array of friends JSONS
+var potentialFriends = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
+exports = potentialFriends;
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 // this will hold the logic for main logic for the separate files
 const express = require("express");
 const path = require("path");
+const htmlRouting = require("./routing/htmlRoutes.js");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+htmlRouting.run(app, path);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
