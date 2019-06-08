603cbe744bb3e04968230804aeafa2c68e9441cc
vlineros <victorlineros@gmail.com>
2019-06-07 23:35:16 -0500

    added style and have app working v.1

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const apiRouting = require("./routing/apiRoutes.js");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
